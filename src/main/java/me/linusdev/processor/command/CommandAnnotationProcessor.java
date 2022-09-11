9c8d89fbe6bf5123e24127c44ef81bed058968eb
LinusDev <info.linusdev@gmail.com>
2022-09-11 13:40:52 +0200

    read file first, then only write missing

--- src/main/java/me/linusdev/processor/command/CommandAnnotationProcessor.java
+++ src/main/java/me/linusdev/processor/command/CommandAnnotationProcessor.java
@@ -16,8 +16,8 @@ import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import javax.tools.FileObject;
 import javax.tools.StandardLocation;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
+import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.Set;
 
@@ -25,18 +25,30 @@ import java.util.Set;
  * processes the @Command annotation from lapi and automatically adds the corresponding service providers to the
  * META-INF/services folder
  */
-@SupportedAnnotationTypes("me.linusdev.lapi.api.manager.command.Command")
-@SupportedSourceVersion(SourceVersion.RELEASE_11)
+@SupportedAnnotationTypes(CommandAnnotationProcessor.COMMAND_ANNOTATION_QUALIFIED_NAME)
 public class CommandAnnotationProcessor extends AbstractProcessor {
+
+    public static final String COMMAND_ANNOTATION_QUALIFIED_NAME = "me.linusdev.lapi.api.manager.command.Command";
+    public static final String BASE_COMMAND_QUALIFIED_NAME = "me.linusdev.lapi.api.manager.command.BaseCommand";
+    public static final String SERVICES_FOLDER = "META-INF/services/";
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latest();
+    }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if(roundEnv.processingOver()){
+            //TODO: check if processing worked fine. see https://stackoverflow.com/questions/47779403/annotation-processing-roundenvironment-processingover
+            return true;
+        }
 
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "starting to process command annotations");
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Starting to process @Command annotations");
 
         for(TypeElement annotation : annotations) {
-            if(!annotation.getQualifiedName().contentEquals("me.linusdev.lapi.api.manager.command.Command")) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "annotation " + annotation.getQualifiedName() + " is not @Command annotation!");
+            if(!annotation.getQualifiedName().contentEquals(COMMAND_ANNOTATION_QUALIFIED_NAME)) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Annotation " + annotation.getQualifiedName() + " is not @Command annotation!");
                 return false;
             }
 
@@ -45,50 +57,85 @@ public class CommandAnnotationProcessor extends AbstractProcessor {
 
             for(Element element : elements) {
                 if(element.getKind() != ElementKind.CLASS) {
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "element " + element.getSimpleName() + " is not a class!");
+                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Element " + element.getSimpleName() + " is not a class!");
+                    continue;
+                }
+                TypeElement typeElement = (TypeElement) element;
+                TypeElement baseCommandType = processingEnv.getElementUtils().getTypeElement(BASE_COMMAND_QUALIFIED_NAME);
+
+                if(baseCommandType == null) {
+                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Project is missing BaseCommand class!");
+                    throw new RuntimeException("Project is missing BaseCommand class!");
+                }
+
+                if(!processingEnv.getTypeUtils().isSubtype(typeElement.asType(), baseCommandType.asType())) {
+                    processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "Element '" + typeElement.getQualifiedName()
+                    + "' is no subtype of BaseCommand. It will be skipped.");
                     continue;
                 }
 
-                TypeElement typeElement = (TypeElement) element;
+
                 if(!qualifiedNames.contains(typeElement.getQualifiedName().toString()))
                     qualifiedNames.add(typeElement.getQualifiedName().toString());
             }
 
             for(String name : qualifiedNames) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "correct element: " + name);
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Found element: " + name);
             }
 
-
-            //TODO: first get resource and read the file, then add missing services.
+            ArrayList<String> alreadyContained = new ArrayList<>();
+            BufferedReader reader = null;
             try {
+                FileObject file = processingEnv.getFiler().getResource(StandardLocation.CLASS_OUTPUT, "",
+                        SERVICES_FOLDER + COMMAND_ANNOTATION_QUALIFIED_NAME);
 
-                try {
-                    FileObject file = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "",
-                            "META-INF/services/me.linusdev.lapi.api.manager.command.BaseCommand");
+                reader = new BufferedReader(new InputStreamReader(file.openInputStream()));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    if (!line.isBlank()) alreadyContained.add(line);
+                }
+            } catch (FileNotFoundException | NoSuchFileException exception) {
+                //File does not exist.
 
+            } catch (IOException exception) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Cannot read resource: " + exception);
 
-
-                    Writer writer = file.openWriter();
-                    for (String name : qualifiedNames) writer.append(name);
-                    writer.close();
-
-                } catch (IOException ex) {
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "cannot create Resources!");
-
+            } finally {
+                if(reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {throw new RuntimeException(e);}
                 }
 
-                /*try {
-                    FileObject file = processingEnv.getFiler().getResource(StandardLocation.CLASS_OUTPUT, "",
-                            "META-INF/services/me.linusdev.lapi.api.manager.command.BaseCommand");
+            }
 
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "got file: " + file.getName());
+            Writer writer = null;
+            try {
+                FileObject file = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "",
+                        SERVICES_FOLDER + COMMAND_ANNOTATION_QUALIFIED_NAME);
 
-                } catch (IOException e) {
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "cannot get Resources!");
 
-                }*/
-            }catch (Exception e) {
-                e.printStackTrace();
+
+                writer = file.openWriter();
+                for (String name : qualifiedNames) {
+                    if(!alreadyContained.contains(name)){
+                        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Adding element: " + name);
+                        writer.append(name);
+                    }
+                }
+                writer.close();
+
+            } catch (IOException exception) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Cannot write resource: " + exception);
+                throw new RuntimeException(exception);
+
+            } finally {
+                if(writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {throw new RuntimeException(e);}
+                }
+
             }
 
         }
